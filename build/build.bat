08eac162d3db61bb4e22bfe11f264bb8532f4cbc
Jamie Dixon <jamie@jamie-dixon.co.uk>
2013-08-07 17:03:21 +0100

    Added a very basic console build with unit test runner

---
+++ build/build.bat
@@ -0,0 +1,36 @@
+@Echo Off
+
+cls
+
+set %ErrorLevel% = 0
+
+echo .
+echo ########################################
+echo Building Solution
+echo ########################################
+echo .
+
+MSBuild ../src/GraphVizWrapper.sln /property:Configuration=Release
+
+if %ERRORLEVEL% neq 0 (
+    type fail.txt
+    GOTO EOF
+)
+
+echo .
+echo ########################################
+echo Running Unit Tests
+echo ########################################
+echo .
+
+"%CD%\..\src\packages\NUnit.2.5.10.11092\tools\nunit-console.exe" "%CD%\..\src\GraphVizWrapper.Tests\bin\Release\GraphVizWrapper.Tests.dll"
+
+if %ERRORLEVEL% equ 0 (     
+    type win.txt
+)
+
+if %ERRORLEVEL% neq 0 (
+    type fail.txt
+)
+
+:EOF
